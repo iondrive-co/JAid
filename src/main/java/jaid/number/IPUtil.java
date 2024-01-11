d9e76a01e35d2411b9eb9516f5411bb5862a9169
Miles <dm@iondrive.co>
2024-01-12 01:43:50 +1100

    New method for packing IP address ranges
    This allows a collection of CIDR addresses to be turned into a format that can be quickly searched via searchRanges.

--- src/main/java/jaid/number/IPUtil.java
+++ src/main/java/jaid/number/IPUtil.java
@@ -1,21 +1,13 @@
 package jaid.number;
 
-public class IPUtil {
+import jaid.collection.Tuples.IntIntPair;
 
-    /**
-     * Checks if the given IPv4 address is within the specified subnet.
-     *
-     * @param ipAddress the IP address as an int.
-     * @param subnet    the subnet address as an int.
-     * @param netmask   the netmask as an int.
-     * @return true if the IP address is within the subnet, false otherwise.
-     */
-    public static boolean isInSubnet(int ipAddress, int subnet, int netmask) {
-        // An XOR between the IP address and the subnet sets the bits that are different between the two to 1.
-        // An AND operation with the netmask checks if any of the bits that are set to 1 fall outside the range
-        // specified by the netmask. If none do (the result is 0) then the IP address is in the subnet.
-        return ((ipAddress ^ subnet) & netmask) == 0;
-    }
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class IPUtil {
 
     /**
      * Calculates the broadcast address of a subnet.
@@ -50,6 +42,21 @@ public class IPUtil {
     }
 
     /**
+     * Checks if the given IPv4 address is within the specified subnet.
+     *
+     * @param ipAddress the IP address as an int.
+     * @param subnet    the subnet address as an int.
+     * @param netmask   the netmask as an int.
+     * @return true if the IP address is within the subnet, false otherwise.
+     */
+    public static boolean isInSubnet(int ipAddress, int subnet, int netmask) {
+        // An XOR between the IP address and the subnet sets the bits that are different between the two to 1.
+        // An AND operation with the netmask checks if any of the bits that are set to 1 fall outside the range
+        // specified by the netmask. If none do (the result is 0) then the IP address is in the subnet.
+        return ((ipAddress ^ subnet) & netmask) == 0;
+    }
+
+    /**
      * Checks if the given IP address is the broadcast address of the subnet.
      *
      * @param ipAddress the IP address as an int.
@@ -79,11 +86,43 @@ public class IPUtil {
      * @param ipAddress the IP address in dotted decimal notation.
      * @return the IP address as an integer.
      */
-    public static int ipAddressToInt(String ipAddress) {
-        String[] parts = ipAddress.split("\\.");
+    public static int ipAddressToInt(final String ipAddress) {
+        final String[] parts = ipAddress.split("\\.");
         return (Integer.parseInt(parts[0]) << 24) |
                 (Integer.parseInt(parts[1]) << 16) |
                 (Integer.parseInt(parts[2]) << 8) |
                 Integer.parseInt(parts[3]);
+    }
+
+    /**
+     * Converts a mask length (in the range 0 - 32) into its subnet mask
+     */
+    public static int maskLengthToNetmask(final String maskLength) {
+        // Integer.parseInt is inefficient given we know maskLength is 1 or 2 digits, so convert to an int ourselves
+        final int maskLengthInt;
+        if (maskLength.length() == 1) {
+            maskLengthInt = maskLength.charAt(0) - '0';
+        } else {
+            maskLengthInt = (maskLength.charAt(0) - '0') * 10 + (maskLength.charAt(1) - '0');
+        }
+        return -1 << (32 - maskLengthInt);
+    }
+
+    /**
+     * Transforms a list of IPv4 addresses in CIDR format into a binary representation suitable for search via
+     * {@link jaid.collection.CollectionUtil#searchRanges}, including sorting them (however the ranges must be
+     * non-overlapping).
+     */
+    public static List<IntIntPair> packAddresses(final Collection<String> addresses) {
+        return addresses.stream().map(address -> {
+            final String[] addressParts = address.split("/");
+            final int intAddress = ipAddressToInt(addressParts[0]);
+            if (addressParts.length > 1) {
+                final int netmask = maskLengthToNetmask(addressParts[1]);
+                return new IntIntPair(getNetworkAddress(intAddress, netmask), getBroadcastAddress(intAddress, netmask));
+            } else {
+                return new IntIntPair(intAddress, intAddress);
+            }
+        }).sorted(Comparator.comparingInt(IntIntPair::first)).collect(Collectors.toList());
     }
 }
