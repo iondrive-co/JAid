a1bd0fa4e7083db40001ff308106321dad35ab0a
mileshampson <dm@iondrive.co>
2025-02-26 00:26:23 +1100

    Clean up JMH vector performance test

--- src/test/java/jaid/collection/DoublesVectorPerformanceTest.java
+++ src/test/java/jaid/collection/DoublesVectorPerformanceTest.java
@@ -9,16 +9,20 @@ import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 
 import java.util.Random;
-import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
-import java.util.stream.DoubleStream;
-import java.util.stream.IntStream;
 
 import static java.util.stream.DoubleStream.generate;
 
 /**
- * Any results in the comments are from an AMD Ryzen 7 6800H laptop.
  * Run in gradle with ./gradlew jmh
+ * Any results in the comments are from an AMD Ryzen 7 6800H laptop.
+ * Benchmark                                                       Mode  Cnt      Score     Error  Units
+ * DoublesVectorPerformanceTest.normalisedDotProductAll            avgt   15   2384.509 ´┐¢  61.092  ns/op
+ * DoublesVectorPerformanceTest.normalisedDotProductSmall          avgt   15      5.659 ´┐¢   0.192  ns/op
+ * DoublesVectorPerformanceTest.normalisedFmaDotProductAll         avgt   15  17439.165 ´┐¢ 137.191  ns/op
+ * DoublesVectorPerformanceTest.normalisedFmaDotProductSmall       avgt   15      4.589 ´┐¢   0.019  ns/op
+ * DoublesVectorPerformanceTest.normalisedMultiplyDotProductAll    avgt   15  13389.219 ´┐¢ 911.029  ns/op
+ * DoublesVectorPerformanceTest.normalisedMultiplyDotProductSmall  avgt   15      5.120 ´┐¢   0.385  ns/op
  */
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
 public class DoublesVectorPerformanceTest {
@@ -48,24 +52,12 @@ public class DoublesVectorPerformanceTest {
         }
     }
 
-    /**
-     * Result "jaid.collection.DoublesVectorPerformanceTest.normalisedDotProductSmall":
-     *   5.698 ´┐¢(99.9%) 0.171 ns/op [Average]
-     *   (min, avg, max) = (5.477, 5.698, 5.896), stdev = 0.160
-     *   CI (99.9%): [5.527, 5.870] (assumes normal distribution)
-     */
     @Benchmark
     @BenchmarkMode(Mode.AverageTime)
     public void normalisedDotProductSmall(TestState testState, Blackhole resultConsumer) {
         resultConsumer.consume(testState.normalisedFirst[0].dotProduct(testState.normalisedSecond[0]));
     }
 
-    /**
-     * Result "jaid.collection.DoublesVectorPerformanceTest.normalisedDotProductAll":
-     *   2381.113 ´┐¢(99.9%) 28.970 ns/op [Average]
-     *   (min, avg, max) = (2301.166, 2381.113, 2445.583), stdev = 38.674
-     *   CI (99.9%): [2352.143, 2410.082] (assumes normal distribution)
-     */
     @Benchmark
     @BenchmarkMode(Mode.AverageTime)
     public void normalisedDotProductAll(TestState testState, Blackhole resultConsumer) {
@@ -75,24 +67,12 @@ public class DoublesVectorPerformanceTest {
         }
     }
 
-    /**
-     * Result "jaid.collection.DoublesVectorPerformanceTest.normalisedDotProductSmall":
-     *   4.614 ´┐¢(99.9%) 0.021 ns/op [Average]
-     *   (min, avg, max) = (4.574, 4.614, 4.649), stdev = 0.020
-     *   CI (99.9%): [4.593, 4.636] (assumes normal distribution)
-     */
     @Benchmark
     @BenchmarkMode(Mode.AverageTime)
     public void normalisedFmaDotProductSmall(TestState testState, Blackhole resultConsumer) {
         resultConsumer.consume(testState.normalisedFirstFma[0].dotProduct(testState.normalisedSecondFma[0]));
     }
 
-    /**
-     * Result "jaid.collection.DoublesVectorPerformanceTest.normalisedDotProductAll":
-     *   17437.131 ´┐¢(99.9%) 64.608 ns/op [Average]
-     *   (min, avg, max) = (17330.221, 17437.131, 17549.885), stdev = 60.435
-     *   CI (99.9%): [17372.522, 17501.739] (assumes normal distribution)
-     */
     @Benchmark
     @BenchmarkMode(Mode.AverageTime)
     public void normalisedFmaDotProductAll(TestState testState, Blackhole resultConsumer) {
@@ -102,24 +82,12 @@ public class DoublesVectorPerformanceTest {
         }
     }
 
-    /**
-     * Result "jaid.collection.DoublesVectorPerformanceTest.normalisedMultiplyDotProductSmall":
-     *   5.258 ´┐¢(99.9%) 0.424 ns/op [Average]
-     *   (min, avg, max) = (4.678, 5.258, 5.665), stdev = 0.397
-     *   CI (99.9%): [4.833, 5.682] (assumes normal distribution)
-     */
     @Benchmark
     @BenchmarkMode(Mode.AverageTime)
     public void normalisedMultiplyDotProductSmall(TestState testState, Blackhole resultConsumer) {
         resultConsumer.consume(testState.normalisedFirstMultiply[0].dotProduct(testState.normalisedSecondMultiply[0]));
     }
 
-    /**
-     * Result "jaid.collection.DoublesVectorPerformanceTest.normalisedMultiplyDotProductAll":
-     *   13561.563 ´┐¢(99.9%) 247.475 ns/op [Average]
-     *   (min, avg, max) = (13177.615, 13561.563, 14023.692), stdev = 231.488
-     *   CI (99.9%): [13314.088, 13809.038] (assumes normal distribution)
-     */
     @Benchmark
     @BenchmarkMode(Mode.AverageTime)
     public void normalisedMultiplyDotProductAll(TestState testState, Blackhole resultConsumer) {
@@ -137,6 +105,34 @@ public class DoublesVectorPerformanceTest {
                 .include(DoublesVectorPerformanceTest.class.getSimpleName())
                 .build();
         new Runner(opt).run();
+    }
+
+    /**
+     * Used to performance test the FMA intrinsic dot product performance
+     */
+    public record FmaIntrinsicDoublesVector (double[] contents) {
+
+        public double dotProduct(final FmaIntrinsicDoublesVector comparedTo) {
+            double sum = 0;
+            for (int i = 0; i < contents.length; ++i) {
+                sum = Math.fma(contents[i], comparedTo.contents[i], sum);
+            }
+            return sum;
+        }
+    }
+
+    /**
+     * Used to performance test the multiply dot product performance
+     */
+    public record MultipleDoublesVector(double[] contents) {
+
+        public double dotProduct(final MultipleDoublesVector comparedTo) {
+            double sum = 0;
+            for (int i = 0; i < contents.length; i++) {
+                sum += contents[i] * comparedTo.contents[i];
+            }
+            return sum;
+        }
     }
 }
 
